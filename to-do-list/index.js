aec8ae18a0d38f9501eadc07f0aab8f7a5435d1d
Anushree Mehta <anushreemehta6@gmail.com>
2026-05-17 11:34:03 +0530

    added light and dark mode functionality

--- to-do-list/index.js
+++ to-do-list/index.js
@@ -2,15 +2,30 @@ let data = document.querySelector(".date-today");
 
 data.textContent = new Date().toLocaleDateString();
 
-let usertodo = document.querySelector(".todos"); 
+let usertodo = document.querySelector(".todos");
 let todo = document.querySelector(".todo");
 let button = document.querySelector(".btn");
-button.addEventListener("click", function (event){
-    event.preventDefault();
-    const todos = todo.value.trim();
-     let li = document.createElement("li");
-     li.textContent= todos;
-    usertodo.append(li);
-      todo.value = "";
-})
+let mode = 'light';
+let modeBtn = document.querySelector(".mode");
+let body = document.querySelector("body");
+button.addEventListener("click", function (event) {
+  event.preventDefault();
+  const todos = todo.value.trim();
+  let li = document.createElement("li");
+  li.textContent = todos;
+  usertodo.append(li);
+  todo.value = "";
+});
 
+modeBtn.addEventListener("click",()=>{
+  if(mode === 'light'){
+   mode ='dark';
+   body.classList.add('dark')
+   body.classList.remove('light')
+  }
+  else{
+   mode = 'light';
+    body.classList.add('light')
+    body.classList.remove('dark')
+  }
+})
